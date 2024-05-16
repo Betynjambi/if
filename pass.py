67519c5d9af75f60f4681c789de65f206a378317
Betynjambi <mutungabett@gmail.com>
2024-05-16 22:58:51 +0300

    pass statement is a null statement which can be used as aplaceholder for future code

---
+++ pass.py
@@ -0,0 +1,17 @@
+# pass statement is a null statement which can be used as aplaceholder for future code
+# using pass with conditional statement
+n = 10
+# use pass inside if statement
+if n > 10:
+    pass
+print('Hello')
+
+# the difference between a comment and a pass statement in Python is that while the interpreter ignores a comment entirely, pass is not ignored
+
+
+# pass statement inside function or class
+def function(args):
+    pass
+
+class Example:
+    pass
